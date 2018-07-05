383638fc0e026a15cc3bf8cc257cef50b3498ca7
N. Keijzer <n.keijzer@student.utwente.nl>
2018-07-05 17:26:59 +0200

    Remove redundant files

--- src/attack_converter.py
+++ src/attack_converter.py
@@ -24,30 +24,30 @@ def main(argv):
     with open(inputFile) as f:
         inputData = json.load(f)
     protocol = inputData['protocol']
-    srcIps = inputData['src_ips'] if maxPropertyLength > len(inputData['src_ips']) > 0 else "any"
-    srcPorts = [int(x) for x in inputData['src_ports']] if maxPropertyLength > len(inputData['src_ports']) > 0 else "any"
-    dstPorts = [int(x) for x in inputData['dst_ports']] if maxPropertyLength > len(inputData['dst_ports']) > 0 else "any"
+    srcIps = '/32,'.join(inputData['src_ips']) + '/32' if maxPropertyLength > len(inputData['src_ips']) > 0 else "any"
+    srcPorts = ','.join(str(int(s)) for s in inputData['src_ports']) if maxPropertyLength > len(inputData['src_ports']) > 0 else "any"
+    dstPorts = ','.join(str(int(s)) for s in inputData['dst_ports']) if maxPropertyLength > len(inputData['dst_ports']) > 0 else "any"
     if protocol == 'DNS':
         query = inputData['additional']['dns_query'].encode('utf-8').hex()
-        dnsType = inputData['additional']['dns_type'].encode('utf-8').hex()
+        dnsType = str(inputData['additional']['dns_type']).encode('utf-8').hex()
         # append dnsType to query because the dns type always directly follows the query in the payload.
-        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"DNS DDoS alert"; classtype:denial-of-service; content:"|{}{}|"; nocase; sid:10000001;)'.format(ruleType, 'udp', '/32,'.join(srcIps) + '/32', ','.join(str(s) for s in srcPorts), ','.join(str(d) for d in dstPorts), query, dnsType)
+        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"DNS DDoS alert"; classtype:denial-of-service; content:"|{}{}|"; nocase; sid:10000001;)'.format(ruleType, 'udp', srcIps, srcPorts, dstPorts, query, dnsType)
     elif protocol == 'ICMP':
-        itype = inputData['additional']['icmp_type']
-        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"ICMP DDoS alert"; classtype:denial-of-service; itype: {}; sid:10000001;)'.format(ruleType, protocol, '/32,'.join(srcIps) + '/32', ','.join(str(s) for s in srcPorts), ','.join(str(d) for d in dstPorts), itype)
+        itype = int(inputData['additional']['icmp_type'])
+        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"ICMP DDoS alert"; classtype:denial-of-service; itype: {}; sid:10000001;)'.format(ruleType, protocol, srcIps, srcPorts, dstPorts, itype)
     elif protocol == 'UDP':
-        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"UDP DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, '/32,'.join(srcIps) + '/32', ','.join(str(s) for s in srcPorts), ','.join(str(d) for d in dstPorts))
+        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"UDP DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, srcIps, srcPorts, dstPorts)
     elif protocol == 'TCP':
         tcpFlag = inputData['additional']['tcp_flag']
-        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"TCP DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, '/32,'.join(srcIps) + '/32',','.join(str(s) for s in srcPorts), ','.join(str(d) for d in dstPorts))
+        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"TCP DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, srcIps, srcPorts, dstPorts)
     elif protocol == 'NTP':
-        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"NTP DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, '/32,'.join(srcIps) + '/32', ','.join(str(s) for s in srcPorts), ','.join(str(d) for d in dstPorts))
+        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"NTP DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, srcIps, srcPorts, dstPorts)
     elif protocol == 'IPv4':
         fragmentation = inputData['additional']['fragmentation']
-        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"IPv4 DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, '/32,'.join(srcIps) + '/32', ','.join(str(s) for s in srcPorts), ','.join(str(d) for d in dstPorts))
+        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"IPv4 DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, srcIps, srcPorts, dstPorts)
     elif protocol == 'QUIC':
         payload = inputData['additional']['quic_payload']
-        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"QUIC DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, '/32,'.join(srcIps) + '/32', ','.join(str(s) for s in srcPorts), ','.join(str(d) for d in dstPorts))
+        rule = '{} {} [{}] {} -> $HOME_NET {} (msg:"QUIC DDoS alert"; classtype:denial-of-service; sid:10000001;)'.format(ruleType, protocol, srcIps, srcPorts, dstPorts)
     print(rule)
     with open(outputFile, "w+") as o:
         o.write(rule + "\r\n")
